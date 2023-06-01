9ea23d50216f616dd6a90e1c4d6ba209295aeae0
orrtaler <74877044+orrtaler@users.noreply.github.com>
2023-06-01 14:17:06 +0300

    finish

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -1,9 +1,11 @@
 const indexR = require("./index");
 const userR = require("./users");
 const cakeR = require("./cakes");
+const countryR = require("./countries");
 
 exports.routesInit = (app) => {
     app.use("/",indexR);
     app.use("/users", userR);
     app.use("/cakes", cakeR);
+    app.use("/countries", countryR);
 }
